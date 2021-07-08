3abc14d7e2a20208a259be1b57ddc36775c7c2d7
Hemingway <ghostblac@protonmail.com>
2021-07-07 20:26:20 -0400

    Add files via upload

---
+++ coin_flipper.py
@@ -0,0 +1,19 @@
+import random
+
+
+class Coins():
+    """
+    """
+
+    def one_coin_flip(self):
+        """
+        """
+
+        sides = [
+            "heads",
+            "tails"
+        ]
+
+        result = random.choice(sides)
+
+        return result

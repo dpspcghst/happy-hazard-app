9191c905b36cf8d4d478b75a070eaf8e1b5a9b30
Hemingway <ghostblac@protonmail.com>
2021-07-07 20:27:36 -0400

    Add files via upload

---
+++ lucille_clifton.py
@@ -0,0 +1,88 @@
+import random
+
+
+class Fullness():
+    """
+    """
+
+    def nineteen_ninety_four(self):
+        """
+        """
+
+        nineteen_ninety_four = [
+            "i was leaving my fifty-eighth year",
+            "when a thumb of ice",
+            "stamped itself hard near my heart",
+            "you have your own story",
+            "you know about the fears the tears",
+            "the scar of disbelief",
+            "you know that the saddest lies",
+            "are the ones we tell ourselves",
+            "you know how dangerous it is",
+            "to be born with breasts",
+            "you know how dangerous it is",
+            "to wear dark skin",
+            "i was leaving my fifty-eighth year",
+            "when i woke into the winter",
+            "of a cold and mortal body",
+            "thin icicles hanging off",
+            "the one mad nipple weeping",
+            "have we not been good children",
+            "did we not inherit the earth",
+            "but you must know all about this",
+            "from your own shivering life"
+        ]
+
+        line = random.choice(nineteen_ninety_four)
+
+        return line
+
+    def blessing_the_boats(self):
+        """
+        """
+
+        blessing_the_boats = [
+            "may the tide",
+            "that is entering even now",
+            "the lip of our understanding",
+            "carry you out",
+            "beyond the face of fear",
+            "may you kiss",
+            "the wind then turn from it",
+            "certain that it will"
+            "love your back     may you",
+            "open your eyes to water",
+            "water waving forever",
+            "and may you in your innocence",
+            "sail through this to that"
+        ]
+
+        line = random.choice(blessing_the_boats)
+
+        return line
+
+    def cutting_greens(self):
+        """
+        """
+
+        cutting_greens = [
+            "curling them around",
+            "i hold their bodies in obscene embrace",
+            "thinking of everything but kinship.",
+            "collards and kale",
+            "strain against each strange other",
+            "away from my kissmaking hand and",
+            "the iron bedpot.",
+            "the pot is black,",
+            "the cutting board is black,",
+            "my hand,",
+            "and just for a minute",
+            "the greens roll black under the knife,",
+            "and the kitchen twists dark on its spine",
+            "and I taste in my natural appetite",
+            "the bond of live things everywhere."
+        ]
+
+        line = random.choice(cutting_greens)
+
+        return line
